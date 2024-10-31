2912f0e4ad38b337c41d50e5bfb1a8c0febba5f2
Farrokh Labib <farrokh@unitary.fund>
2024-10-31 15:40:54 +0100

    adding files

---
+++ helper_functions.py
@@ -0,0 +1,229 @@
+import time
+
+import numpy as np
+import perceval as pcvl
+import exqalibur
+from typing import List, Tuple, Dict, Union
+
+
+H_matrix = (1/np.sqrt(2)) * pcvl.Matrix([[1.0, 1.0], [1.0, -1.0]])
+M_matrix = (1/np.sqrt(2)) * pcvl.Matrix([[1.0, 1.0], [1.0j, -1.0j]])
+mzi = (pcvl.BS() // (0, pcvl.PS(pcvl.Parameter("phi1"))) // 
+        pcvl.BS() // (0, pcvl.PS(pcvl.Parameter("phi2"))))
+H_circ = pcvl.Circuit.decomposition(H_matrix, mzi, shape=pcvl.InterferometerShape.TRIANGLE)
+M_circ = pcvl.Circuit.decomposition(M_matrix, mzi, shape=pcvl.InterferometerShape.TRIANGLE)
+
+
+def is_qubit_state(state: exqalibur.FockState) -> Union[Tuple[int, ...], bool]:
+    """
+    Check if a given state is a valid qubit state.
+
+    Args:
+    state (exqalibur.FockState): The state to check
+
+    Returns:
+    Union[Tuple[int, ...], bool]: The corresponding qubit state if valid, False otherwise
+    """
+    q_state = []
+    for i in range((state.m - 1) // 2):
+        q = state[2*i+1 : 2*i+3]
+        if (q[0] == 0 and q[1] == 1):
+            q_state.append(1)
+        elif (q[0] == 1 and q[1] == 0):
+            q_state.append(0)
+        else:
+            return False
+    return tuple(q_state)
+
+# def qubit_state_marginal(res: List[exqalibur.FockState]) -> Dict[Tuple[int, ...], float]:
+#     """
+#     Calculate the frequencies of measured qubit states from sampled Fock states.
+
+#     Args:
+#     res (List[exqalibur.FockState]): Sampled Fock states
+
+#     Returns:
+#     Dict[Tuple[int, ...], float]: Frequencies of measured qubit states
+#     """
+#     q_state_frequency = {}
+#     total_measurements = 0
+    
+#     for state in res:
+#         q_state = is_qubit_state(state)
+#         if q_state:
+#             total_measurements += 1
+#             q_state_frequency[q_state] = q_state_frequency.get(q_state, 0) + 1
+    
+#     return {k: v / total_measurements for k, v in q_state_frequency.items()}
+
+def qubit_state_marginal(prob_dist):
+    """
+    given sampled Fock states, return the frequencies
+    of measured qubit states.
+    """
+    q_state_frequency = {}
+    total_prob_mass = 0
+    for state in prob_dist:
+        # print(state)
+        q_state = is_qubit_state(state)
+        if q_state is not False:
+            total_prob_mass += prob_dist[state]
+            q_state_frequency[q_state] = prob_dist[state]
+    
+    for key in q_state_frequency.keys():
+        q_state_frequency[key] /= total_prob_mass
+    
+    return q_state_frequency
+
+def compute_energy(pauli_bin: Tuple[int, ...], res: Dict[Tuple[int, ...], float]) -> float:
+    """
+    Compute the expectation value for a given Pauli string and measurement results.
+
+    Args:
+    pauli_bin (Tuple[int, ...]): A tuple of 0's and 1's (0's are identities, 1's are non-identities (X or Z))
+    res (Dict[Tuple[int, ...], float]): Frequencies of measured qubit bitstrings
+
+    Returns:
+    float: The corresponding expectation value
+    """
+    for key in res.keys():
+        inner = np.dot(key, pauli_bin)
+        sign = (-1)**inner
+        res[key] *= sign
+    
+    return np.sum(np.fromiter(res.values(), dtype=float), where=np.isfinite)
+
+def pauli_string_bin(pauli_string: str) -> Tuple[int, ...]:
+    """
+    Convert a Pauli string to a binary representation.
+
+    Args:
+    pauli_string (str): A string representation of Pauli operators (e.g., "IXZI")
+
+    Returns:
+    Tuple[int, ...]: Binary representation of the Pauli string
+    """
+    return tuple(0 if c == "I" else 1 for c in pauli_string)
+
+def rotate_qubits(pauli_string: str, vqe_circuit: pcvl.Circuit) -> pcvl.Circuit:
+    """
+    Apply the correct rotations on corresponding qubits for expectation value computation.
+
+    Args:
+    pauli_string (str): A string representation of Pauli operators
+    vqe_circuit (pcvl.Circuit): The VQE circuit to modify
+
+    Returns:
+    pcvl.Circuit: The modified VQE circuit with applied rotations
+    """
+    
+    for i, pauli in enumerate(pauli_string):
+        qubit = (2*i+1, 2*i+2)
+        if pauli == "X":
+            vqe_circuit.add(qubit, H_circ)
+        elif pauli == "Y":
+            vqe_circuit.add(qubit, M_circ)
+    
+    return vqe_circuit
+
+def hamiltonian_dictionary(h: np.ndarray) -> Dict[str, float]:
+    """
+    Convert a 2-qubit Hamiltonian from array form to a dictionary.
+
+    Args:
+    h (np.ndarray): 2-qubit Hamiltonian in array form
+
+    Returns:
+    Dict[str, float]: Dictionary with Pauli string keys and coefficient values
+    """
+    pauli_strings = ["II", "IX", "IZ", "XI", "XX", "XZ", "ZI", "ZX", "ZZ"]
+    return dict(zip(pauli_strings, h))
+
+def loss_function(lp: np.ndarray, List_Parameters: List[pcvl.Parameter], 
+                  ansatz: pcvl.Circuit, H: Dict[str, float]) -> float:
+    """
+    Compute the loss function for the VQE algorithm.
+
+    Args:
+    lp (np.ndarray): Array of parameter values
+    List_Parameters (List[pcvl.Parameter]): List of Perceval parameters
+    ansatz (pcvl.Circuit): The ansatz circuit
+    H (Dict[str, float]): Hamiltonian dictionary
+
+    Returns:
+    float: The computed loss value
+    """
+    for p, value in zip(List_Parameters, lp):
+        p.set_value(value)
+
+    loss = 0.0
+    for pauli_string, coefficient in H.items():
+        start = time.time()
+        ansatz_rot = rotate_qubits(pauli_string, ansatz.copy())
+        # start = time.time()
+        end = time.time()
+        processor = pcvl.Processor(pcvl.NaiveBackend(), ansatz_rot)
+        processor.with_input(pcvl.BasicState([0,1,0,1,0,0]))  # Hardcoded for now
+        
+        # print(f'rotation time: {end-start}')
+        
+        start = time.time()
+        sampler = pcvl.algorithm.Sampler(processor)
+        samples = sampler.samples(100_000)
+        end = time.time()
+        # print(f'sample time: {end-start}')
+
+        
+        # res = samples['results'] #TODO: get frequencies immediately
+        prob_dist = sampler.probs()['results']
+        pauli_bin = pauli_string_bin(pauli_string)
+        start = time.time()
+        expectation = compute_energy(pauli_bin, qubit_state_marginal(prob_dist))
+        loss += coefficient * expectation
+        end = time.time()
+        # print(f'loss compute time: {end-start}')
+    
+    return loss
+
+def ansatz() -> Tuple[pcvl.Circuit, List[pcvl.Parameter]]:
+    """
+    Create the ansatz circuit for the VQE algorithm.
+
+    Returns:
+    Tuple[pcvl.Circuit, List[pcvl.Parameter]]: The ansatz circuit and list of parameters
+    """
+    List_Parameters = [pcvl.Parameter(f"φ{i}") for i in range(1, 9)]
+    VQE = pcvl.Circuit(6)
+
+    # First layer
+    VQE.add((1,2), pcvl.BS())
+    VQE.add((3,4), pcvl.BS())
+    VQE.add((2,), pcvl.PS(phi=List_Parameters[0]))
+    VQE.add((4,), pcvl.PS(phi=List_Parameters[2]))
+    VQE.add((1,2), pcvl.BS())
+    VQE.add((3,4), pcvl.BS())
+    VQE.add((2,), pcvl.PS(phi=List_Parameters[1]))
+    VQE.add((4,), pcvl.PS(phi=List_Parameters[3]))
+
+    # CNOT (Post-selected with a success probability of 1/9)
+    VQE.add([0,1,2,3,4,5], pcvl.PERM([0,1,2,3,4,5]))
+    VQE.add((3,4), pcvl.BS())
+    VQE.add([0,1,2,3,4,5], pcvl.PERM([0,1,2,3,4,5]))
+    VQE.add((0,1), pcvl.BS(pcvl.BS.r_to_theta(1/3)))
+    VQE.add((2,3), pcvl.BS(pcvl.BS.r_to_theta(1/3)))
+    VQE.add((4,5), pcvl.BS(pcvl.BS.r_to_theta(1/3)))
+    VQE.add([0,1,2,3,4,5], pcvl.PERM([0,1,2,3,4,5]))
+    VQE.add((3,4), pcvl.BS())
+    VQE.add([0,1,2,3,4,5], pcvl.PERM([0,1,2,3,4,5]))
+
+    # Second layer
+    VQE.add((2,), pcvl.PS(phi=List_Parameters[4]))
+    VQE.add((4,), pcvl.PS(phi=List_Parameters[6]))
+    VQE.add((1,2), pcvl.BS())
+    VQE.add((3,4), pcvl.BS())
+    VQE.add((2,), pcvl.PS(phi=List_Parameters[5]))
+    VQE.add((4,), pcvl.PS(phi=List_Parameters[7]))
+    VQE.add((1,2), pcvl.BS())
+    VQE.add((3,4), pcvl.BS())
+
+    return VQE, List_Parameters
