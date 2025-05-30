5ad306d0a360feda0f215c8bc9e206f173ca1a02
Kitsunp <154612767+Kitsunp@users.noreply.github.com>
2025-05-30 05:21:25 -0600

    Update __init__.py

--- qlass/quantum_chemistry/__init__.py
+++ qlass/quantum_chemistry/__init__.py
@@ -1,8 +1,10 @@
 from .classical_solution import hamiltonian_matrix, brute_force_minimize
-from .hamiltonians import LiH_hamiltonian
+from .hamiltonians import LiH_hamiltonian, generate_random_hamiltonian, LiH_hamiltonian_tapered
 
 __all__ = [
     "hamiltonian_matrix",
     "brute_force_minimize",
     "LiH_hamiltonian",
+    "generate_random_hamiltonian", 
+    "LiH_hamiltonian_tapered"
 ]
