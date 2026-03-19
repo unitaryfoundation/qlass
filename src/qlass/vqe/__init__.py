ac49dd6c3f0a5be9031195808e95b2214ab5e8c1
FarLab <farrokh@unitary.fund>
2026-03-19 15:56:52 +0100

    added kerr ansatz

--- src/qlass/vqe/__init__.py
+++ src/qlass/vqe/__init__.py
@@ -1,8 +1,9 @@
-from .ansatz import custom_unitary_ansatz, le_ansatz
+from .ansatz import custom_unitary_ansatz, kerr_ansatz, le_ansatz
 from .vqe import VQE
 
 __all__ = [
     "le_ansatz",
     "custom_unitary_ansatz",
+    "kerr_ansatz",
     "VQE",
 ]
